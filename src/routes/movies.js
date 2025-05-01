e601840d4c9336b7dc5271db35aa6b39b5ad0b71
yashnagar67 <nitwalababaji@gmail.com>
2025-05-01 16:27:13 +0530

    adding global and india

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -27,7 +27,9 @@ Recommended: getModelByCollection('Recommended'),
 massmania: getModelByCollection('massmania'),
 massmania2: getModelByCollection('massmania2'),
 massmania3: getModelByCollection('massmania3'),
-massmania4: getModelByCollection('massmania4')
+massmania4: getModelByCollection('massmania4'),
+topglobal: getModelByCollection('topglobal'),
+topindia: getModelByCollection('topindia'),
 
   
 };
